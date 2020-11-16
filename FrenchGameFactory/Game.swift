b2bfc4bd28321c1a29a57432a7ff01aa1eacc9e8
Keltoum BELKADI <keltoum.belkadi@gmail.com>
2020-11-16 15:35:57 +0100

    Player, Game propreties and methods

--- FrenchGameFactory/Game.swift
+++ FrenchGameFactory/Game.swift
@@ -13,99 +13,176 @@ import Foundation
 
 // Définir les accès
 class Game {
-    var players : [Player] = [Player]()
-    let maxPlayers = 2
-
-
+    private var player1: Player?
+    private var player2 : Player?
+    
+    
     
     func Intro(){
         print("Bienvenue à Crash of Inquisition. \nVous participez à un jeu de rôle au tour par tous où vous vous affrontrez l'un contre l'autre. \nLe vainqueur est celui qui restera en vie.\n \nAlors c'est parti!!!\n")
     }
     
-    func createPlayer() {
+//    func createPlayer() {
+//
+//        print( "Quel est le nom de ton équipe?")
+//        let namePlayer = Tools.shared.getInputString()
+//        // let pOne = Player(namePlayer: namePlayer)
+//        //       players.append(pOne)
+//
+//    }
     
-            print( "Quel est le nom de ton équipe?")
-            let namePlayer = Tools.shared.getInputString()
-            let pOne = Player(namePlayer: namePlayer)
-            players.append(pOne)
-        
-      
-    }
-
-   func startGame(){
-    // definir les joueurs et stocker les joueurs
-    //créer le joueur 1 et 2 et demander de créer la chaque joueur en appelant la méthode de createTeams.
+    func startGame(){
+        // definir les joueurs et stocker les joueurs
+        //créer le joueur 1 et 2 et demander de créer la chaque joueur en appelant la méthode de createTeams.
         Intro()
-        createPlayer()
-        createTeams()
-}
-    
-   
-    
-    func createTeams(){
-      // avec les joueurs créer lancer la méthode selectTeams de la classe Player
-        var characterAlive = players[0].characterAlive
-
-               for _ in 0 ... 2{
-                   print("Veuillez selectionner la classe de votre personnage: "
-                   + "\n1. Templier, arme : épée."
-                   + "\n2. Nain, arme : hâche."
-                   + "\n3. Elf, arme : arc."
-                   + "\n4. Mage, arme : sort d'attaque.")
-
-                   let strChoice = Tools.shared.getInputInt()
-
-                   switch strChoice{
-                       case 1 :
-                            print("Veuillez choisir le nom de votre templier?")
-                            let charaName = Tools.shared.getInputString()
-                            let templar = Templar(name : charaName)
-                            characterAlive.append(templar)
-
-                       case 2 :
-                           //le meme code que templar
-                            print("Veuillez choisir le nom de votre nain?")
-                            let charaName = Tools.shared.getInputString()
-                            let dwarf = Dwarf(name : charaName)
-                            characterAlive.append(dwarf)
-
-                       case 3 :
-                           //le meme code que templar
-                            print("Veuillez choisir le nom de votre elf?")
-                            let charaName = Tools.shared.getInputString()
-                            let elf = Elf(name : charaName)
-                            characterAlive.append(elf)
-
-                       case 4 :
-                           //le meme code que templar
-                            print("Veuillez choisir le nom de votre mage?")
-                            let charaName = Tools.shared.getInputString()
-                            let magus = Magus(name : charaName)
-                            characterAlive.append(magus)
-
-                       default :
-                            print("Je ne comprends pas.")
-
-                   }
-
-
-           }
-
-
-
+        //createPlayer()
+        //createTeams()
     }
+    
+    func choiceTeam(name: String) -> Characters {
+        var teamNumber = 0
+        
+        print("Veuillez selectionner la classe de votre personnage: "
+                + "\n1. Templier, arme : épée."
+                + "\n2. Nain, arme : hâche."
+                + "\n3. Elf, arme : arc."
+                + "\n4. Mage, arme : sort d'attaque.")
+        
+        repeat {
+            let team = Tools.shared.getInputInt()
+            
+            switch team {
+            case 1 :
+                teamNumber += 1
+                let templar = Templar(name : name)
+                print(templar.name, templar.weapon.nameWeapon, templar.weapon.damage)
+                return templar
+            // characterAlive.append(templar)
+            
+            case 2 :
+                teamNumber += 1
+                //le meme code que templar
+                let dwarf = Dwarf(name : name)
+                return dwarf
+            // characterAlive.append(dwarf)
+            
+            case 3 :
+                teamNumber += 1
+                //le meme code que templar
+                let elf = Elf(name : name)
+                return elf
+            // characterAlive.append(elf)
+            
+            case 4 :
+                teamNumber += 1
+                //le meme code que templar
+                let magus = Magus(name : name)
+                return magus
+            //  characterAlive.append(magus)
+            default:
+                print("you didn't choose a character")
+            }
+        } while teamNumber < 1
+    }
+    
+    func askNameOfCharacter() {
+        var tabNamesOfCharacters: [String] = [String]()
+        var tabOfCharacters: [Characters] = [Characters]()
+        repeat {
+            print("\n Player 1 -> Please choose the name of your character \(tabOfCharacters.count + 1) :")
+            var check: Bool = false
+            repeat {
+                let name = Tools.shared.getInputString()
+                if !tabNamesOfCharacters.contains(name) {
+                    check = true
+                    tabOfCharacters.append(choiceTeam(name: name))
+                    tabNamesOfCharacters.append(name)
+                } else {
+                    print("\(name) is already taken !")
+                }
+            } while check == false
+        } while tabOfCharacters.count != 3
+        print(tabNamesOfCharacters[0], tabOfCharacters[0].type, tabNamesOfCharacters[1], tabOfCharacters[1].type, tabOfCharacters[2].type, tabNamesOfCharacters[2])
+        player1 = Player(characters: [tabOfCharacters[0], tabOfCharacters[1], tabOfCharacters[2]])
+        
+        repeat {
+            print("\n Player 2 -> Please choose the name of your character \(tabOfCharacters.count + 1) :")
+            var check: Bool = false
+            repeat {
+                let name = Tools.shared.getInputString()
+                if !tabNamesOfCharacters.contains(name) {
+                    check = true
+                    tabOfCharacters.append(choiceTeam(name: name))
+                    tabNamesOfCharacters.append(name)
+                } else {
+                    print("\(name) is already taken !")
+                }
+            } while check == false
+        } while tabOfCharacters.count != 3
+        print(tabNamesOfCharacters[0], tabOfCharacters[0].type, tabNamesOfCharacters[1], tabOfCharacters[1].type, tabOfCharacters[2].type, tabNamesOfCharacters[2])
+        player2 = Player(characters: [tabOfCharacters[0], tabOfCharacters[1], tabOfCharacters[2]])
+    }
+    
+//    func createTeams(){
+//        // avec les joueurs créer lancer la méthode selectTeams de la classe Player
+//        // var characterAlive = players[0].characterAlive
+//
+//        for _ in 0 ... 2{
+//            print("Veuillez selectionner la classe de votre personnage: "
+//                    + "\n1. Templier, arme : épée."
+//                    + "\n2. Nain, arme : hâche."
+//                    + "\n3. Elf, arme : arc."
+//                    + "\n4. Mage, arme : sort d'attaque.")
+//
+//            let strChoice = Tools.shared.getInputInt()
+//
+//            switch strChoice{
+//            case 1 :
+//                print("Veuillez choisir le nom de votre templier?")
+//                let charaName = Tools.shared.getInputString()
+//                let templar = Templar(name : charaName)
+//            // characterAlive.append(templar)
+//
+//            case 2 :
+//                //le meme code que templar
+//                print("Veuillez choisir le nom de votre nain?")
+//                let charaName = Tools.shared.getInputString()
+//                let dwarf = Dwarf(name : charaName)
+//            // characterAlive.append(dwarf)
+//
+//            case 3 :
+//                //le meme code que templar
+//                print("Veuillez choisir le nom de votre elf?")
+//                let charaName = Tools.shared.getInputString()
+//                let elf = Elf(name : charaName)
+//            // characterAlive.append(elf)
+//
+//            case 4 :
+//                //le meme code que templar
+//                print("Veuillez choisir le nom de votre mage?")
+//                let charaName = Tools.shared.getInputString()
+//                let magus = Magus(name : charaName)
+//            //  characterAlive.append(magus)
+//
+//            default :
+//                print("Je ne comprends pas.")
+//
+//            }
+//        }
+//
+//    }
     
     func StartBattle () {
         //ajouter les methodes attack et heal et transférer les perso morts dans le tableau charatersDead
-
+        
     }
-
+    
     func displayWinner(){
         
     }
-
+    
     // mettre en place le coffret aléatoire dans le jeu
-   
+    
 }
 
 
--- FrenchGameFactory/Player.swift
+++ FrenchGameFactory/Player.swift
@@ -10,41 +10,43 @@ import Foundation
 //      ACTIONS DES JOUEURS - CHOIX DES PERSONNAGES
 //------------------------------------------------------------------
 // pour chaque personnages choisi un nom à donner , mais également limiter les équipe à 3 (tableau) on peut a choisir des personnages de la meme classe.
-    
-    
-   public class Player{
-    
-    
-    var namePlayer : String
-    var characterAlive : [Characters] = [Characters]()
-    var characterDead :[Characters] = [Characters]()
-    
-    init(namePlayer : String){
-        self.namePlayer = namePlayer
-    }
 
 
-   
+ class Player {
     
-   
+    var characterAlive: [Characters] = [Characters]()
+    var characterDead: [Characters] = [Characters]()
+    var numberOfCharacterDies: Int = 0
     
     
-        func heal /*(Characters.lifePoints :Int )*/(){
-            
-//Creer une variable qui va stocker les points de vies des personnages apres les attaques mais aussi les soins ?
-            //if Characters.lifePoint < Characters.lifePoint de depart{
-//                Characters.lifePoint += 10
-//            }else if  Characters.lifePont ==0{
-//                characterDead.append(<#T##newElement: Game.Characters##Game.Characters#>)
-//                characterAlive.remove(at: <#T##Int#>) comment indiquer ou supprimer
-//               print ("\(charater.name) est mort et ne pas être soigné, séléctionner un autre membre de l'équipe")
-//
-//            }else print("votre joueur est en pleine santé")
-    }
-    func attack(){
-        // lors de l'attaque sur une personnage associer la force avec les points de dommages.
-        //mettre en place des case pour selection le personnage à attquer?
-      
+    init(characters: [Characters]) {
+        characters.forEach { (character) in
+            self.characterAlive.append(character)
         }
+    }
+    
+    func printCharacterInLife() {
+        characterAlive.enumerated().forEach { (index, character) in
+            print("\(index + 1) : \(character.name) (\(character.type) \(character.lifePoint) LP - (\(character.weapon.damage) WD)")
+        }
+    }
+    
+//    func heal /*(Characters.lifePoints :Int )*/(){
+//
+//        //Creer une variable qui va stocker les points de vies des personnages apres les attaques mais aussi les soins ?
+//        //if Characters.lifePoint < Characters.lifePoint de depart{
+//        //                Characters.lifePoint += 10
+//        //            }else if  Characters.lifePont ==0{
+//        //                characterDead.append()
+//        //                characterAlive.remove(at: ) comment indiquer ou supprimer
+//        //               print ("\(charater.name) est mort et ne pas être soigné, séléctionner un autre membre de l'équipe")
+//        //
+//        //            }else print("votre joueur est en pleine santé")
+//    }
+//    func attack(){
+//        // lors de l'attaque sur une personnage associer la force avec les points de dommages.
+//        //mettre en place des case pour selection le personnage à attquer?
+//
+//    }
     
 }
