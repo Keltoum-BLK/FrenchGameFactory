4caf370c3115ab19952cee5f6ad438094c27dda7
Keltoum BELKADI <keltoum.belkadi@gmail.com>
2020-12-15 19:25:39 +0100

    correct comments added

--- FrenchGameFactory/Charaters.swift
+++ FrenchGameFactory/Charaters.swift
@@ -20,16 +20,17 @@ class Characters{
         self.lifePoint = lifePoint
         self.weapon = weapon
     }
-    
+    //MARK: FUNCTION ATTACK A PLAYER WITH A TARGET
     func attack(player : Player, target : Characters){
         
       target.lifePoint -= weapon.damage
         print ("\(target.name)'s life point are \(target.lifePoint).")
-        
+        //command to send a character with a negatif or zero life point at the characterDead array.
         if target.lifePoint <= 0{
             target.lifePoint = 0
             print("\(target.name) is dead ⚰️.")
             player.numberOfCharacterDies += 1
+            //removeAll check the whole array to find the same name in the characterAlive array with de name of the target.
             player.characterAlive.removeAll { (character) -> Bool in
                 return target.name == character.name ? true : false
             }
